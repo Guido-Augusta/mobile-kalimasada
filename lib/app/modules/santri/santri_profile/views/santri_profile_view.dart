23ffce68cfed0f4be1e392cb04e3592f643628c5
Guido-Augusta <muhammadguido29@gmail.com>
2025-10-17 15:45:28 +0700

    feat: no induk in santri profile

--- lib/app/modules/santri/santri_profile/views/santri_profile_view.dart
+++ lib/app/modules/santri/santri_profile/views/santri_profile_view.dart
@@ -1084,6 +1084,14 @@ class SantriProfileView extends GetView<SantriProfileController> {
           const SizedBox(height: 16),
 
           _buildInfoTile(
+            icon: Icons.credit_card,
+            label: 'No. Induk',
+            value: santri.noInduk ?? '-',
+          ),
+
+          const SizedBox(height: 12),
+
+          _buildInfoTile(
             icon: Icons.email,
             label: 'Email',
             value: santri.user?.email ?? 'Tidak ada data',
