bc33373ebc2960986d56644d40d3c28a00b95bac
Guido-Augusta <muhammadguido29@gmail.com>
2025-11-03 10:40:13 +0700

    feat: login validator

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,11 +1,15 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:toastification/toastification.dart';
 
 class LoginController extends GetxController {
+  final formKey = GlobalKey<FormState>();
+
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
@@ -27,47 +31,130 @@ class LoginController extends GetxController {
 
   /// Handle login process
   void callLoginApi() async {
-    if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
-      try {
-        final response = await post(
-          Uri.parse('http://10.0.2.2:5000/api/auth/login'),
-          body: jsonEncode({
-            'email': emailController.text,
-            'password': passwordController.text,
-            'platform': 'mobile',
-          }),
-          headers: {'Content-Type': 'application/json'},
+    try {
+      final response = await post(
+        Uri.parse('http://10.0.2.2:5000/api/auth/login'),
+        body: jsonEncode({
+          'email': emailController.text,
+          'password': passwordController.text,
+          'platform': 'mobile',
+        }),
+        headers: {'Content-Type': 'application/json'},
+      );
+
+      var data = jsonDecode(response.body);
+      print(response.statusCode);
+      print(data);
+
+      if (response.statusCode == 200) {
+        final SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString('token', data['token'].toString());
+        await prefs.setString('role', data['user']['role'].toString());
+        await prefs.setString('userId', data['user']['id'].toString());
+        await prefs.setString('roleId', data['user']['roleId'].toString());
+        toastification.show(
+          context: Get.context!,
+          title: Wrap(
+            alignment: WrapAlignment.center,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            children: [
+              Icon(Icons.check_circle_outline_rounded, color: Colors.green),
+              SizedBox(width: 10),
+              Text('Login berhasil', style: TextStyle(color: Colors.white)),
+            ],
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          icon: Icon(Icons.check_circle_outline_rounded, color: Colors.green),
+          showIcon: true,
+          backgroundColor: Color(0xFF6B6B6B),
+          borderSide: BorderSide.none,
+          alignment: Alignment.bottomCenter,
+          autoCloseDuration: const Duration(milliseconds: 2000),
+          closeButton: ToastCloseButton(showType: CloseButtonShowType.none),
+          animationBuilder: (context, animation, alignment, child) {
+            return FadeTransition(opacity: animation, child: child);
+          },
+          type: ToastificationType.success,
+          style: ToastificationStyle.simple,
         );
-
-        var data = jsonDecode(response.body);
-        print(response.statusCode);
-        print(data);
-
-        if (response.statusCode == 200) {
-          final SharedPreferences prefs = await SharedPreferences.getInstance();
-          await prefs.setString('token', data['token'].toString());
-          await prefs.setString('role', data['user']['role'].toString());
-          await prefs.setString('userId', data['user']['id'].toString());
-          await prefs.setString('roleId', data['user']['roleId'].toString());
-          Get.snackbar('Success', 'Login berhasil');
-          // Navigate to home or another page
-          if (data['user']['role'] == 'santri') {
-            Get.offAllNamed('/santri-main');
-          } else if (data['user']['role'] == 'ustadz') {
-            Get.offAllNamed('/ustadz-main');
-          } else if (data['user']['role'] == 'ortu') {
-            Get.offAllNamed('/ortu-main');
-          } else {
-            Get.offAllNamed('/home');
-          }
+        // Navigate to home or another page
+        if (data['user']['role'] == 'santri') {
+          Get.offAllNamed('/santri-main');
+        } else if (data['user']['role'] == 'ustadz') {
+          Get.offAllNamed('/ustadz-main');
+        } else if (data['user']['role'] == 'ortu') {
+          Get.offAllNamed('/ortu-main');
         } else {
-          Get.snackbar('Error', data['message'] ?? 'Login gagal');
+          Get.offAllNamed('/home');
         }
-      } catch (e) {
-        Get.snackbar('Error', 'An error occurred: $e');
+      } else {
+        toastification.show(
+          context: Get.context!,
+          title: Wrap(
+            alignment: WrapAlignment.center,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            children: [
+              Icon(Icons.error, color: Colors.white),
+              SizedBox(width: 10),
+              Text('Login gagal', style: TextStyle(color: Colors.white)),
+            ],
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          icon: Icon(Icons.error, color: Colors.white),
+          showIcon: true,
+          backgroundColor: Color(0xFF6B6B6B),
+          borderSide: BorderSide.none,
+          alignment: Alignment.bottomCenter,
+          autoCloseDuration: const Duration(milliseconds: 2000),
+          closeButton: ToastCloseButton(showType: CloseButtonShowType.none),
+          animationBuilder: (context, animation, alignment, child) {
+            return FadeTransition(opacity: animation, child: child);
+          },
+          type: ToastificationType.error,
+          style: ToastificationStyle.simple,
+        );
       }
-    } else {
-      Get.snackbar('Error', 'Email and password tidak boleh kosong');
+    } catch (e) {
+      toastification.show(
+        context: Get.context!,
+        title: Wrap(
+          alignment: WrapAlignment.center,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: [
+            Icon(Icons.error, color: Colors.white),
+            SizedBox(width: 10),
+            Text(
+              'Terjadi kesalahan saat menambahkan hafalan\nPeriksa koneksi internet Anda',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        icon: Icon(Icons.error, color: Colors.white),
+        showIcon: true,
+        backgroundColor: Color(0xFF6B6B6B),
+        borderSide: BorderSide.none,
+        alignment: Alignment.bottomCenter,
+        autoCloseDuration: const Duration(milliseconds: 2000),
+        closeButton: ToastCloseButton(showType: CloseButtonShowType.none),
+        animationBuilder: (context, animation, alignment, child) {
+          return FadeTransition(opacity: animation, child: child);
+        },
+        type: ToastificationType.error,
+        style: ToastificationStyle.simple,
+      );
     }
+  }
+
+  String? validateEmail(String? email) {
+    RegExp emailRegex = RegExp(
+      r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+    );
+    if (email == null || email.isEmpty) {
+      return 'Email tidak boleh kosong';
+    } else if (!emailRegex.hasMatch(email)) {
+      return 'Masukkan email yang valid';
+    }
+    return null;
   }
 }
