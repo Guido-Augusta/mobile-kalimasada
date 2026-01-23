9966e0658959e61a2ec215dfabd36ddc53d7c51b
Guido-Augusta <muhammadguido29@gmail.com>
2026-01-23 15:11:19 +0700

    fix: doa khatam texts

--- lib/app/modules/doa_khatam/controllers/doa_khatam_controller.dart
+++ lib/app/modules/doa_khatam/controllers/doa_khatam_controller.dart
@@ -3,11 +3,9 @@ import 'package:get/get.dart';
 class DoaKhatamController extends GetxController {
   // Data doa khatam Al-Qur'an
   final String doaArab =
-      "اللَّهُمَّ ارْحَمْنِي بِالْقُرْآنِ وَاجْعَلْهُ لِي إِمَامًا وَنُورًا وَهُدًى وَرَحْمَةً، اللَّهُمَّ ذَكِّرْنِي مِنْهُ مَا نُسِّيتُ وَعَلِّمْنِي مِنْهُ مَا جَهِلْتُ وَارْزُقْنِي تِلَاوَتَهُ آنَاءَ اللَّيْلِ وَأَطْرَافَ النَّهَارِ وَاجْعَلْهُ لِي حُجَّةً يَا رَبَّ الْعَالَمِينَ";
-
+      "اللَّهُمَّ ارْحَمْنَا بِالقُرْءَانِ وَاجْعَلْهُ لَنَا إِمَامًا وَنُورًا وَهُدًا وَرَحْمَةً، اللَّهُمَّ ذَكِّرْنَا مِنْهُ مَا نَسِينَا وَعَلِّمْنَا مِنْهُ مَا جَهِلْنَا وَارْزُقْنَا تِلَاوَتَهُ ءَانَآءَ الَّيْلِ وَأَطْرَافَ النَّهَارِ وَاجْعَلْهُ لَنَا حُجَّةً يَا رَبَّ الْعَالَمِينَ";
   final String doaLatin =
-      "Allhummarhamni bilquran. Waj'alhu lii imaman wa nuran wa hudan wa rohmah. Allhumma dzakkirni minhu maa nasiitu wa 'allimnii minhu maa jahiltu warzuqnii tilawatahu aana-allaili wa'atrofannahaar waj'alhu li hujatan ya rabbal 'alamin";
-
+      "Allahummarhamna bil qur'an. Waj'alhulana imaaman wa nuuran wa hudan wa rahmah. Allahumma dzakkirna minhuma nasiina Wa 'allimna minhumaa jahilna Warzuqnaa tilaawatahu 'aana 'allayli wa 'athraa fannahar Waj'alhu lana hujjatan yaa Rabbal-'alamiin.";
   final String doaTerjemahan =
-      "Ya Allah, rahmatilah aku dengan Al-Quran. Jadikanlah ia sebagai pemimpin, cahaya, petunjuk, dan rahmat bagiku. Ya Allah, ingatkanlah aku atas apa yang terlupakan darinya. Ajarilah aku atas apa yang belum tahu darinya. Berikanlah aku kemampuan membacanya sepanjang malam dan ujung siang. Jadikanlah ia sebagai pembelaku, wahai tuhan semesta alam.";
+      "Ya Allah, rahmatilah kami dengan Al-Quran. Jadikanlah ia sebagai pemimpin, cahaya, petunjuk, dan rahmat bagi kami. Ya Allah, ingatkanlah kami atas apa yang kami lupa. Dan ajarkan kepada kami atas apa yang kami tidak tahu. Karuniakan kepada kami untuk dapat membacanya sepanjang malam dan sepanjang siang. Jadikanlah ia sebagai perisai kami, wahai Tuhan semesta alam.";
 }
